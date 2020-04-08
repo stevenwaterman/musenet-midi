9dc55e77635fea172f58f0d80f1d2bef6777f46c
Steven Waterman <github@stevenwaterman.uk>
2020-04-08 20:36:23 +0100

    Update distributed version

---
+++ index.d.ts
@@ -0,0 +1,4 @@
+import { fromMidiToMusenet } from "./lib/fromMidi";
+import { fromMusenetToMidi } from "./lib/toMidi";
+export declare const fromMidi: typeof fromMidiToMusenet;
+export declare const toMidi: typeof fromMusenetToMidi;
